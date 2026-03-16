f12113c2ab6ddead5050117fd8092d1ec130630f
Sayeedur Rahman <97797507+avonoeia@users.noreply.github.com>
2026-03-16 17:54:14 +0600

    api/shorten-url: database store logic

--- server.js
+++ server.js
@@ -6,9 +6,31 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
 
-app.get('/api/', (req, res) => {
-    res.send(`<h1>Hi, World!</h1>`)
+app.post('/api/shorten-url', async (req, res) => {
+    const { alias, long_url } = req.body;
+    
+    // Store into database
+    try {
+        let _ = await db.query('INSERT INTO mapping (alias, long_url) VALUES (?, ?)', [alias, long_url]);
+        res.status(200).json({ 
+            'message': 'URL creation successful',
+            'short_url': `http://localhost:${PORT}/${alias}` 
+        });
+    }
+    catch (err) {
+        console.error('Error shortening URL:', err);
+        
+        let message = 'An error occurred while shortening the URL.';
+        if (err.code === 'ER_DUP_ENTRY') {
+            message = 'The provided alias already exists. Please choose a different alias.';
+        }
+        res.status(500).json({ 
+            'message': message,
+            'error': 'Internal Server Error' 
+        });
+    }
 });
 
 app.listen(PORT, () => {
