c4daa603e730f391bc20941e4df0d2803bed927d
Sayeedur Rahman <97797507+avonoeia@users.noreply.github.com>
2026-04-15 09:03:55 +0600

    VPS database

--- db/connectionPool.js
+++ db/connectionPool.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
 
 // Create the connection pool
-// const pool = mysql.createPool({
-//   host: process.env.VPS_IP,
-//   user: process.env.VPS_DB_USER,
-//   password: process.env.VPS_DB_PASSWORD,
-//   database: 'tinyurl',
-//   waitForConnections: true,
-//   connectionLimit: 10, // Max number of concurrent connections
-//   queueLimit: 0
-// });
-const pool = mysql.createPool(process.env.TIDB_URI);
+const pool = mysql.createPool({
+  host: process.env.VPS_IP,
+  user: process.env.VPS_DB_USER,
+  password: process.env.VPS_DB_PASSWORD,
+  database: 'tinyurl',
+  waitForConnections: true,
+  connectionLimit: 10, // Max number of concurrent connections
+  queueLimit: 0
+});
+// const pool = mysql.createPool(process.env.TIDB_URI);
 
 const promisePool = pool.promise();
 
