6c164f56ae5d86ef38c774c1e3dfd000240dd640
Sayeedur Rahman <97797507+avonoeia@users.noreply.github.com>
2026-04-15 07:34:12 +0600

    development deployment build

--- db/connectionPool.js
+++ db/connectionPool.js
@@ -2,15 +2,15 @@ const mysql = require('mysql2');
 
 // Create the connection pool
 const pool = mysql.createPool({
-  host: '103.174.51.157',
-  user: 'render',
-  password: 'render',
+  host: process.env.VPS_IP,
+  user: process.env.VPS_DB_USER,
+  password: process.env.VPS_DB_PASSWORD,
   database: 'tinyurl',
   waitForConnections: true,
   connectionLimit: 10, // Max number of concurrent connections
   queueLimit: 0
 });
-// const pool = mysql.createPool('mysql://2yXMAp3zmNhv22q.root:zIq5i75qGy3GzRfn@gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com:4000/test?ssl={"rejectUnauthorized":true}');
+// const pool = mysql.createPool(process.env.TIDB_URI);
 
 const promisePool = pool.promise();
 
